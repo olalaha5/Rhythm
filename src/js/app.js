2668ba1cbedc9bbf0f5856a4f2e3450c9fc1a088
IlyaOl <81174461+olalaha5@users.noreply.github.com>
2022-07-14 19:00:38 +0300

    Add files via upload

---
+++ src/js/app.js
@@ -0,0 +1,30 @@
+/**/import * as flsFunctions from "./modules/webpChecker.js";
+/**/flsFunctions.isWebp()
+/* код выше не удалять */
+
+
+
+
+document.addEventListener('DOMContentLoaded', () => {
+    let buttons = document.querySelectorAll(".our-customer__slider-button");
+    let items = document.querySelector(".our-customer__slider-items");
+    let item = document.querySelectorAll(".our-customer__slider-item");
+
+    function height(i) {
+        items.style.height = `${item[i].clientHeight}px`;
+        console.log(item[i])
+    };
+    height(0)
+
+    buttons.forEach((elem, i) => {
+        buttons[i].addEventListener('click', (event) => {
+            items.style.transform = `translate(-${i * 100}vw)`;
+            buttons.forEach((elem, j) => {
+                buttons[j].classList.remove("our-customer__slider-button_active");
+            })
+            buttons[i].classList.add("our-customer__slider-button_active");
+            height(i)
+        })
+    })
+
+});
